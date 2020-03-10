946b79620c2f1388919308c76cdfd18d4400ab39
HardikAswal <hardik.aswal@gmail.com>
2020-03-10 15:47:30 +0530

    Removed Auth Token and Account SID

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = 'AC9f2631590b05bd643d4f809dd1003a39'
-auth_token = 'a0dd9b3b49d1a43c2f2448118c955317'
-my_cell = '+919509188860'
-my_twilio = '+16072142980'
+account_sid = 'Account SID'
+auth_token = 'Auth Token'
+my_cell = 'Cell Number'
+my_twilio = 'Twilio Number'
